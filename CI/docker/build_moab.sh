156646bb44a3adcd5b9c60b8234c0d46482d2f86
Paul P.H. Wilson <paul.wilson@wisc.edu>
2021-04-02 08:05:02 -0500

    fix RPATH for shared libs

--- CI/docker/build_moab.sh
+++ CI/docker/build_moab.sh
@@ -15,26 +15,25 @@ mkdir -p ${moab_build_dir}/bld
 cd ${moab_build_dir}
 git clone --depth 1 https://bitbucket.org/fathomteam/moab -b ${branch}
 cd bld
-cmake ../moab -DBUILD_SHARED_LIBS=OFF \
-              -DENABLE_HDF5=ON -DHDF5_ROOT=${hdf5_install_dir} \
+cmake ../moab -DENABLE_HDF5=ON -DHDF5_ROOT=${hdf5_install_dir} \
               -DENABLE_BLASLAPACK=OFF \
               -DENABLE_FORTRAN=OFF \
               -DCMAKE_INSTALL_PREFIX=${moab_install_dir} \
               -DCMAKE_C_COMPILER=${CC} \
               -DCMAKE_CXX_COMPILER=${CXX} \
-              -DCMAKE_INSTALL_RPATH=${hdf5_install_dir}/lib:${moab_install_dir}/lib
+              -DBUILD_SHARED_LIBS=OFF
 make -j${ci_jobs}
 make install
 rm -rf *
-cmake ../moab -DBUILD_SHARED_LIBS=ON \
-              -DENABLE_HDF5=ON -DHDF5_ROOT=${hdf5_install_dir} \
+cmake ../moab -DENABLE_HDF5=ON -DHDF5_ROOT=${hdf5_install_dir} \
               -DENABLE_PYMOAB=ON \
               -DENABLE_BLASLAPACK=OFF \
               -DENABLE_FORTRAN=OFF \
               -DCMAKE_INSTALL_PREFIX=${moab_install_dir} \
               -DCMAKE_C_COMPILER=${CC} \
               -DCMAKE_CXX_COMPILER=${CXX} \
-              -DCMAKE_INSTALL_RPATH=${hdf5_install_dir}:${moab_install_dir}
+              -DBUILD_SHARED_LIBS=ON \
+              -DCMAKE_INSTALL_RPATH=${hdf5_install_dir}/lib:${moab_install_dir}/lib
 make -j${ci_jobs}
 make install
 cd
