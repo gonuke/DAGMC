fcdf2a71defd4f4d809354a46be45acdfd1641e3
Ahnaf Tahmid <ahnaf-tahmid@outlook.com>
2023-03-11 20:30:18 +0600

    back to 10.5.1

--- CI/docker/build_geant4.sh
+++ CI/docker/build_geant4.sh
@@ -3,10 +3,10 @@
 set -ex
 
 # Geant version and corresponding SHASUM
-export geant4_version=11.1.1
+export geant4_version=10.5.1
 export geant4_basename=geant4-v${geant4_version}
 export geant4_tarball=${geant4_basename}.tar.gz
-export geant4_shasum=11ab0665adb4f4f955b0c662a8212a64c972523f47bd9afa45e6c9d1351371b4
+export geant4_shasum=2397eb859dc4de095ff66059d8bda9f060fdc42e10469dd7890946293eeb0e39
 
 source ${docker_env}
 
